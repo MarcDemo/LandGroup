2974f7d72c4d67a06ea4df4f70914eca73aebbb9
MarcDemo <mark.demo.kitumba@gmail.com>
2026-07-15 11:41:05 +0300

    Fix mobile proof preview dismissal

--- groupcore/tests.py
+++ groupcore/tests.py
@@ -1,8 +1,11 @@
 from datetime import date
+from decimal import Decimal
 
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
+from deposits.models import DepositSubmission
 from .models import GroupSettings, MemberProfile
 
 
@@ -24,6 +27,31 @@ class MobileAppShellTests(TestCase):
         self.assertContains(response, '>Deposit</span>', html=False)
         self.assertContains(response, 'sidebar-logout')
         self.assertContains(response, reverse('logout'))
+
+    def test_proof_preview_has_mobile_close_controls(self):
+        member = MemberProfile.objects.create_user(
+            username='proof-member', password='pw', role='MEMBER'
+        )
+        deposit = DepositSubmission.objects.create(
+            member=member,
+            submitted_by=member,
+            starting_week=date(2025, 7, 7),
+            weeks_covered=1,
+            amount=Decimal('20000'),
+            land_savings_amount=Decimal('20000'),
+            payment_date=timezone.localdate(),
+            payment_time=timezone.localtime().time(),
+        )
+        deposit.proof = 'proofs/mobile-proof.jpg'
+        deposit.save(update_fields=['proof'])
+
+        self.client.force_login(member)
+        response = self.client.get(reverse('member_dashboard'))
+
+        self.assertContains(response, f'id="proofModal{deposit.id}"')
+        self.assertContains(response, 'class="btn-close" data-bs-dismiss="modal"')
+        self.assertContains(response, 'data-bs-dismiss="modal">Close</button>')
+        self.assertContains(response, 'modal-dialog-scrollable')
 
     def test_treasurer_gets_role_specific_mobile_actions(self):
         treasurer = MemberProfile.objects.create_user(
